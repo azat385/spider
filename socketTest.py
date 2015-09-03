0ea1f307716caca10eae458d9e26bce8656f0845
azat385 <azat385@gmail.com>
2015-09-03 21:18:09 +0300

    sys.argv[1]=portNum

--- socketTest.py
+++ socketTest.py
@@ -23,14 +23,27 @@ logger.critical('critical message')
 
 logger.info('PROGRAMM IS STARTING...\n\n')
 
+import sys
+
+if len(sys.argv)==1:
+    print "Usage {} portNum".format(sys.argv[0])
+    sys.exit("No port")
+try:
+    port = int(sys.argv[1])
+#if not isinstance(port, int):
+except:
+    print "{} is not a number".format(sys.argv[1])
+    sys.exit("Not a number")
+
 
 import socket
 from time import sleep as sleep
 
 sock = socket.socket()
 sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-sock.bind(('', 14210))
+sock.bind(('', port))
 sock.listen(1)
+logger.info("waiting for connection on {} port...".format(port))
 conn, addr = sock.accept()
 print 'connected:', addr
 
@@ -38,7 +51,7 @@ def makeConnection():
 	global sock, conn
 	sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 	sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-	sock.bind(('', 14210))
+	sock.bind(('', port))
 	sock.listen(1)
 	conn, addr = sock.accept()
 	#print 'connected:', addr
@@ -50,7 +63,7 @@ def requestLoop():
 	#conn.send("\x04\x03\x00\x07\x00\x01\x35\x9E")
 	conn.send("\x04\x03\x00\x00\x00\x0A\xC5\x98")
 	data = conn.recv(1024)
-	#print [hex(ord(d)) for d in data], ord(data[4])
+	#print [hex(ord(d)) for d in data], ord(data[18])
 	dataArray = [ord(d) for d in data]
 	logger.debug("Recieved msg in dec = {}".format(dataArray))
 	logger.info("Inc100 = {:03}".format( ord(data[18]) ))
