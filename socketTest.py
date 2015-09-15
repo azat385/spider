981937a12f40587255090aaf685c91a982e4cdcb
azat385 <azat385@gmail.com>
2015-09-15 10:45:35 +0300

    pack to real

--- socketTest.py
+++ socketTest.py
@@ -4,7 +4,7 @@
 import sys
 
 if len(sys.argv)==1:
-    logger.error("Usage {} portNum".format(sys.argv[0]))
+    logger.error("Usage: {} portNum 0/1(wrxAuth)".format(sys.argv[0]))
     sys.exit("No port")
 try:
     port = int(sys.argv[1])
@@ -12,7 +12,12 @@ try:
 except:
     logger.error("{} is not a number".format(sys.argv[1]))
     sys.exit("Not a number")
-
+try:
+    doWrxAuth = int(sys.argv[2])
+except:
+    print "by default wrxAuh is OFF"
+    doWrxAuth = 0
+print "doWrxAuth={}".format(doWrxAuth )
 
 import logging
 from logging.handlers import TimedRotatingFileHandler
@@ -54,18 +59,26 @@ def wrxAuth():
 	s = []
 	conn.send("\xC0\x00\x06\x00\x14\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\xE7\x48\xC2")
 	#print "Wait for reply..."
-	data = conn.recv(1024)
+	try:
+		data = conn.recv(1024)
+	except socket.error as err:
+		logger.error("Error: Socket error: {}".format(err))
+		return s		
 	#print "Reply:",data
-	dataArray = [ord(d) for d in data]
+	try:
+		dataArray = [ord(d) for d in data]
+		s.append(ord(data[5]))
+		s.append(data[5+1:5+1+15])
+		s.append(data[5+16:5+16+16])
+		s.append(data[5+32:5+32+10])
+		s.append(ord(data[67]))
+	except:
+		logger.error("Error: in response calcualations")
+		return s
 	#print dataArray
 	sleep(3)
 	#print "Send Auth confirmation..."
 	conn.send("\xC0\x00\x06\x01\x14\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x3F\x25\xC2")
-	s.append(ord(data[5]))
-	s.append(data[5+1:5+1+15])
-	s.append(data[5+16:5+16+16])
-	s.append(data[5+32:5+32+10])
-	s.append(ord(data[67]))
 	logger.warn("WRX Auth {}".format(s))
 	return s
 	#sleep(3)
@@ -86,6 +99,7 @@ def makeConnection():
 
 def requestLoop():
     global sock, conn
+    i=0
     while True:
 	#conn.send("\x04\x03\x00\x07\x00\x01\x35\x9E")
 	conn.send("\x04\x03\x00\x00\x00\x0A\xC5\x98")
@@ -98,17 +112,31 @@ def requestLoop():
 		logger.info("Inc100 = {:03}".format(inc100))
 	except:
 		logger.error("Some error in recieved query in dec = {}".format(dataArray))
+	if i==2:
+		raise ValueError("Raise exception to close the connection")
+	i += 1
 	sleep(10)
 
+def printFloat(byteArray,startByte):
+	return struct.unpack(">f",data[startByte:startByte+4][2:4]+data[startByte:startByte+4][0:2])[0]
+#data range(5,100,4)===1...24 floats
+
+def printWord(byteArray,startByte):
+	return struct.unpack(">h",data[startByte:startByte+2])[0]
+#range(101,120,2)===25...34 shorts
+
 #makeConnection()
-wrxAuth()
+if doWrxAuth:
+	wrxAuth()
 while True:
     try:
 	requestLoop()
-    except socket.error:
+    except (socket.error,ValueError) as err:
+        print err
 	conn.close()
 	sleep(15)
 	makeConnection()
-	wrxAuth()
+	if doWrxAuth:
+		wrxAuth()
 
 conn.close()
