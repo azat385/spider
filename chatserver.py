f4d5084e00a2628ee6b5d4f2dd9efb082472951f
Ubuntu <ubuntu@ip-172-31-20-84.eu-central-1.compute.internal>
2016-01-11 13:37:03 +0300

    add logging

--- chatserver.py
+++ chatserver.py
@@ -4,27 +4,28 @@ run me with twistd -y chatserver.py, and then connect with multiple
 telnet clients to port 1025
 """
 port = 6565
-delayBeforeDropConnection = 120
+delayBeforeDropConnection = 180
 
 
 #from twisted.protocols import basic
-from twisted.internet import protocol, reactor
+from twisted.internet import protocol, reactor, error
 from twisted.application import service, internet
-
+from twisted.python import log
 
 
 class MyChat(protocol.Protocol):
     def connectionMade(self):
-        print "Got new client: {}".format(self.transport.getPeer())
+        log.msg( "Got new client: {}".format(self.transport.getPeer()) )
         self.factory.clients.append(self)
 	self.timeout = reactor.callLater(delayBeforeDropConnection, self.dropConnection)
+	self.myError = error
 
     def connectionLost(self, reason):
-        print "Lost a client! reason: {}".format(reason)
+        log.msg( "Lost a client! reason: {}".format(reason) )
         self.factory.clients.remove(self)
 
     def dataReceived(self, line):
-        print "received", repr(line)
+        log.msg( "received {}".format( repr(line) ) )
 	self.timeout.reset(delayBeforeDropConnection)
         for c in self.factory.clients:
             if c != self:
@@ -34,6 +35,7 @@ class MyChat(protocol.Protocol):
         self.transport.write(message)
 
     def dropConnection(self):
+	self.myError.UserError(string='drop connection from server due to silence')
 	self.transport.loseConnection()
 
 
