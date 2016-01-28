7f4ee61142f7a0469059c00b0638d49dc614f108
Ubuntu <ubuntu@ip-172-31-20-84.eu-central-1.compute.internal>
2016-01-28 14:24:56 +0300

    add echo server with auth

--- echoAuthServer.py
+++ echoAuthServer.py
@@ -7,8 +7,8 @@ sudo kill -TERM $(sudo cat chat.pid)
 sudo twistd  --python=chatserver.py --pidfile=chat.pid --logfile=chatlog/chat.log
 
 """
-port = 6565
-delayBeforeDropConnection = 180
+port = 6567
+delayBeforeDropConnection = 300
 
 
 #from twisted.protocols import basic
@@ -16,13 +16,22 @@ from twisted.internet import protocol, reactor, error
 from twisted.application import service, internet
 from twisted.python import log
 
+class auth():
+    Request = "WhoAreYou?"
+    Clients = [ "ImYourClient001\n", "ImYourClient001" ]
+    Confirm = "OKletsSwitchToTrasparentMode"
+    Failed  = "NOT_AUTH"
+    pass
 
 class MyChat(protocol.Protocol):
+    stage = 0
+
     def connectionMade(self):
         log.msg( "Got new client: {}".format(self.transport.getPeer()) )
         self.factory.clients.append(self)
 	self.timeout = reactor.callLater(delayBeforeDropConnection, self.dropConnection)
-	self.myError = error
+	self.stage = 1
+	self.message(auth.Request)
 
     def connectionLost(self, reason):
         log.msg( "Lost a client! reason: {}".format(reason) )
@@ -30,24 +39,31 @@ class MyChat(protocol.Protocol):
 
     def dataReceived(self, line):
         log.msg( "received {}".format( repr(line) ) )
-	self.timeout.reset(delayBeforeDropConnection)
-        for c in self.factory.clients:
-            if c != self:
-		c.message(line)
+	if self.stage == 1:
+	    if line in auth.Clients:
+		self.stage = 2
+		self.message(auth.Confirm)
+	    else:
+		self.message(auth.Failed)
+		#self.stage = 3
+		self.dropConnection()
+	else:
+	    self.timeout.reset(delayBeforeDropConnection)
+            for c in self.factory.clients:
+            	#if c != self:
+	    	# send to all active clients
+	    	c.message(line)
 
     def message(self, message):
         self.transport.write(message)
 
     def dropConnection(self):
-	#self.myError.UserError(string='drop connection from server due to silence')
 	log.msg( "Drop connection from server due to silence{}".format(self.transport.getPeer()) )
 	self.transport.loseConnection()
-
-
 
 factory = protocol.ServerFactory()
 factory.protocol = MyChat
 factory.clients = []
 
-application = service.Application("chatserver")
+application = service.Application("echoServer", uid=0, gid=0, )
 internet.TCPServer(port, factory).setServiceParent(application)
