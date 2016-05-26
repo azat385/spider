add227299fa162931d2e62c0f4bbcb7dc7a6f6b3
azat385 <azat385@gmail.com>
2016-05-26 15:06:29 +0300

    backup name

--- indexer.py
+++ indexer.py
@@ -5,6 +5,7 @@
 #from plotHTML import base_path
 base_path = "/home/ubuntu/spider/html/"
 file_name = "index.html"
+file_name_sans_ext = file_name.split(".")[0]
 
 from datetime import date
 time_str = date.today()
@@ -16,5 +17,5 @@ print all_header_files
 
 import shutil
 for f_old in all_header_files:
-    f_new = f_old.replace(file_name, time_str)
+    f_new = f_old.replace(file_name_sans_ext, time_str)
     shutil.copy(f_old, f_new)
