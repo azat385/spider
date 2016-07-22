470325d1a8abab99dcf4f85a1f1291b7e92d1215
azat385 <azat385@gmail.com>
2016-07-22 08:46:00 +0300

    try to test db

--- plotHTML.py
+++ plotHTML.py
@@ -1,6 +1,24 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+from datetime import datetime, date, timedelta
+ts = str(datetime.now())
+db_name = 'test2.db'
+from peewee import *
+
+db = SqliteDatabase(db_name)
+
+class RAWDATA(Model):
+    NAME = TextField()
+    VALUE = FloatField()
+    STIME = CharField(max_length=50)
+
+    class Meta:
+        database = db
+
+
+arc_prefix = "archive_"
+
 import sqlite3
 from datetime import datetime
 
@@ -12,11 +30,11 @@ conn.text_factory = str
 
 
 p = [
-    'Давл_нагн',
-    'Темп_камеры',
-    't_всас',
-    'Темп_охлад',
-    't_комн_конд'
+    {'key': 'Давл_нагн',    'name': 'Р нагнетания'},
+    {'key': 'Темп_камеры',  'name': 'Т камеры'},
+    {'key': 't_всас',       'name': 'Т всас фреон'},
+    {'key': 'Темп_охлад',   'name': 'Т охладителя'},
+    {'key': 't_комн_конд',  'name': 'Т комн конденс'},
 ]
 
 f = [
@@ -36,15 +54,16 @@ f = [
     "Pвсас_2",
 ]
 std_pxl_arc = [
-    "SCo_t_(наружная)",
-    "SCo_t_(канала)",
-    "SCo_t_(обр_воды)",
-    "SCo_t_(помещения)",
-    "SCo_t_(вытяжки)",
-    "SCo_Уставка_t",
-    "SCo_%ВП",
-    "SCo_%ВВ",
+    {'key': "SCo_t_(наружная)",     'name': 'T наружняя'},
+    {'key': "SCo_t_(канала)",       'name': 'T притока'},
+    {'key': "SCo_t_(обр_воды)",     'name': 'T обр воды'},
+    {'key': "SCo_t_(помещения)",    'name': 'T вытяжки'},
+    {'key': "SCo_t_(вытяжки)",      'name': 'T выброса'},
+    {'key': "SCo_Уставка_t",        'name': 'T уставка'},
+    {'key': "SCo_%ВП",              'name': '%% ВП'},
+    {'key': "SCo_%ВВ",              'name': '%% ВВ'},
 ]
+
 add_aktanish_PP1_arc = [
     "FT1_ppm",
     "UT1_h",
@@ -66,7 +85,7 @@ add_aktanish_PP1_arc = [
     "UT5_H",
 ]
 add_aktanish_PP1_arc = std_pxl_arc + add_aktanish_PP1_arc
-arc_prefix = "archive_"
+
 
 
 def form_dict(name="morg.", prefix=arc_prefix, data_list=std_pxl_arc):
@@ -93,43 +112,53 @@ settings = [
     form_dict(name="test.PV2_"),
 ]
 
-for ss in settings:
-    p = ss['data']
-    f = []
-    for p1 in p:
-        c1 = conn.execute("SELECT  VALUE, STIME FROM RAWDATA WHERE NAME=? AND (STIME>= datetime('now','-1 day')) ORDER BY ID DESC",(p1,))
-        f.append(c1.fetchall())
 
-    if not f:
-        continue
-    data = []
-    for p1, f1 in zip(p, f):
-        x1 = []
-        y1 = []
-        for f2 in f1:
-            x1.append(f2[0])
-            y1.append(datetime.strptime(f2[1],"%Y-%m-%d %H:%M:%S.%f"))
-        #>>> lst=[[1,2,3],[11,12,13],[21,22,23]]
-        #>>> zip(*lst)[0]
-        #(1, 11, 21)
-        cool_str_in_graph = p1.replace(ss['tag_prefix'], "")
-        cool_str_in_graph = cool_str_in_graph.replace("SCo_", "")
-        trace = dict(name=cool_str_in_graph, y=x1, x=y1)
-        data.append(trace)
+def form_one_graph(common_name="morg.", prefix=arc_prefix, data_dict=std_pxl_arc):
+    return
 
-    from plotly.graph_objs import Scatter, Layout
-    from plotly.offline import plot
-    add_path = ss['path']
-    full_path = "{}{}".format(base_path,add_path)
-    import os
-    if not os.path.isdir(full_path):
-        os.makedirs(full_path)
-    plot(data, filename='{}index.html'.format(full_path), auto_open=False, show_link=False)
-
-conn.close()
 
 if __name__ == '__main__':
-    pass
+    exit()
+    #previous
+    for ss in settings:
+        p = ss['data']
+        f = []
+        # SELECT "t1"."id", "t1"."NAME", "t1"."VALUE", "t1"."STIME"
+        # FROM "rawdata" AS t1
+        # WHERE (("t1"."NAME" LIKE ?) AND ("t1"."STIME" >= ?))
+        # ORDER BY "t1"."NAME"
+        # LIMIT 500 [u'archive_morg.%', u'2016-06-27 12:55:12.130000']
+        for p1 in p:
+            c1 = conn.execute("SELECT  VALUE, STIME FROM RAWDATA WHERE NAME=? AND (STIME>= datetime('now','-1 day')) ORDER BY ID DESC",(p1,))
+            f.append(c1.fetchall())
+
+        if not f:
+            continue
+        data = []
+        for p1, f1 in zip(p, f):
+            x1 = []
+            y1 = []
+            for f2 in f1:
+                x1.append(f2[0])
+                y1.append(datetime.strptime(f2[1],"%Y-%m-%d %H:%M:%S.%f"))
+            #>>> lst=[[1,2,3],[11,12,13],[21,22,23]]
+            #>>> zip(*lst)[0]
+            #(1, 11, 21)
+            cool_str_in_graph = p1.replace(ss['tag_prefix'], "")
+            cool_str_in_graph = cool_str_in_graph.replace("SCo_", "")
+            trace = dict(name=cool_str_in_graph, y=x1, x=y1)
+            data.append(trace)
+
+        from plotly.graph_objs import Scatter, Layout
+        from plotly.offline import plot
+        add_path = ss['path']
+        full_path = "{}{}".format(base_path,add_path)
+        import os
+        if not os.path.isdir(full_path):
+            os.makedirs(full_path)
+        plot(data, filename='{}index.html'.format(full_path), auto_open=False, show_link=False)
+
+    conn.close()
 
 
 
--- writeDB.py
+++ writeDB.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 from mcStat import getKeys
+mcKeyLimit = 1000
 
 import memcache
 mc = memcache.Client(['127.0.0.1:11211'], debug=0)
@@ -60,16 +61,71 @@ def check_table_exist():
         return False
 
 if __name__ == '__main__':
+    from datetime import datetime, date, timedelta
+    ts = str(datetime.now())
+    db_name = 'test2.db'
+    from peewee import *
+
+    db = SqliteDatabase(db_name)
+
+    class RAWDATA(Model):
+        NAME = TextField()
+        VALUE = FloatField()
+        STIME = CharField(max_length=50)
+
+        class Meta:
+            database = db # This model uses the "people.db" database.
+
+    #RAWDATA.create_table(fail_silently=True)
+
+    #RAWDATA.create(NAME="tag01", VALUE=330.33, STIME=ts)
+    #RAWDATA.create(NAME="tag02", VALUE=331.30, STIME=ts)
+    #RAWDATA.create(NAME="tag03", VALUE=332.30, STIME=ts)
+
+    key = "archive_sterlitamak.RT22_SCo_t_(обр_воды)"
+    arr = (
+             ("archive_sterlitamak.RT22_SCo_t_(обр_воды)", 20.7532024384, "2016-06-28 12:10:25.587980"),
+             ("archive_sterlitamak.RT22_SCo_t_(обр_воды)", 20.7156429291, "2016-06-28 12:15:38.381105"),
+             ("archive_sterlitamak.RT22_SCo_t_(обр_воды)", 20.7156429291, "2016-06-28 12:20:41.424295"),
+             ("archive_sterlitamak.RT22_SCo_t_(обр_воды)", 20.7156429291, "2016-06-28 12:25:43.121070"),
+             ("archive_sterlitamak.RT22_SCo_t_(обр_воды)", 20.6405220032, "2016-06-28 12:30:53.396369"),
+             ("archive_sterlitamak.RT22_SCo_t_(обр_воды)", 20.6655635834, "2016-06-28 12:36:14.181619"),
+             ("archive_sterlitamak.RT22_SCo_t_(обр_воды)", 20.5528831482, "2016-06-28 12:41:39.534234"),
+             ("archive_sterlitamak.RT22_SCo_t_(обр_воды)", 20.4902820587, "2016-06-28 12:47:22.491720"),
+             ("archive_sterlitamak.RT22_SCo_t_(обр_воды)", 20.5278434753, "2016-06-28 12:52:30.615641"),
+             ("archive_sterlitamak.RT22_SCo_t_(обр_воды)", 20.5153217316, "2016-06-28 12:57:39.587488"),
+             ("archive_sterlitamak.RT22_SCo_t_(обр_воды)", 20.5153217316, "2016-06-28 13:02:44.328145"),
+             ("archive_sterlitamak.RT22_SCo_t_(обр_воды)", 20.4902820587, "2016-06-28 13:07:49.428175"),
+             ("archive_sterlitamak.RT22_SCo_t_(обр_воды)", 20.5153217316, "2016-06-28 13:12:56.932940"),
+             ("archive_sterlitamak.RT22_SCo_t_(обр_воды)", 20.4527225494, "2016-06-28 13:18:07.808674"),
+             ("archive_sterlitamak.RT22_SCo_t_(обр_воды)", 20.4276828766, "2016-06-28 13:23:11.533018"),
+             ("archive_sterlitamak.RT22_SCo_t_(обр_воды)", 20.6405220032, "2016-06-28 13:28:11.801325"),
+             ("archive_sterlitamak.RT22_SCo_t_(обр_воды)", 20.6530437469, "2016-06-28 13:33:12.968851"),
+             ("archive_sterlitamak.RT22_SCo_t_(обр_воды)", 20.6906032562, "2016-06-28 13:38:13.768756"),
+    )
+
+    #RAWDATA.insert_many().execute()
+
+    r1 = RAWDATA\
+        .select()\
+        .where((RAWDATA.NAME ** "archive_morg.Темп%") & (RAWDATA.STIME >= datetime.now()-timedelta(hours=3)))\
+        .order_by(RAWDATA.NAME)\
+        .limit(500)
+
+    print r1
+
+    for r in r1:
+        print r.id, r.NAME, r.VALUE, r.STIME
     #create_table()
     #write_data_to_db()
-    #exit()
-    db_name = 'test1.db'
+    exit()
+
     import sqlite3
     #conn = sqlite3.connect(db_name)
     from time import sleep
 
     while 1:
-        top100keys = getKeys(limit=200)
+        top100keys = getKeys(limit=mcKeyLimit)
         for key in top100keys:
             if "archive" in key:
                 for _ in xrange(5):
